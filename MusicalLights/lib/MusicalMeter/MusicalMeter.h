245a3fd36cdf84b08618c97fefab5d09cfa4a72d
Juan Moreno <jmoreno.0312@gmail.com>
2019-02-14 01:43:28 -0800

    Only one space for access keywords; added #define guard.

--- MusicalLights/lib/MusicalMeter/MusicalMeter.h
+++ MusicalLights/lib/MusicalMeter/MusicalMeter.h
@@ -1,22 +1,25 @@
+#ifndef MUSICAL_METER_H_
+#define MUSICAL_METER_H_
+
 #define AVERAGE_COUNT_BUFFER_SIZE 3
 
 class MusicalMeter {
+ public:
+  MusicalMeter(uint8_t numberOfLevels, uint8_t startingPin);
+  void CycleLevels(uint32_t delay_between_levels);
+  void DisplayAdaptive(uint16_t audio_level);
+  void Display(uint16_t audio_level);
+  void SetZeroLevel(uint16_t zero_level);
 
-  public:
-    MusicalMeter(uint8_t numberOfLevels, uint8_t startingPin);
-    void CycleLevels(uint32_t delay_between_levels);
-    void DisplayAdaptive(uint16_t audio_level);
-    void Display(uint16_t audio_level);
-    void SetZeroLevel(uint16_t zero_level);
-
-  private:
-    uint16_t zero_level_;
-    uint8_t *led_pins_;
-    uint8_t number_of_levels_;
-    uint16_t current_max_;
-    uint16_t average_buffer_[AVERAGE_COUNT_BUFFER_SIZE];
-    uint8_t average_index_;
-    void WriteToBuffer(uint16_t level);
-    uint16_t GetBufferAverage();
-    void DisplayAudioLevelBasic(uint16_t audio_level, uint16_t max_value);
+ private:
+  uint16_t zero_level_;
+  uint8_t *led_pins_;
+  uint8_t number_of_levels_;
+  uint16_t current_max_;
+  uint16_t average_buffer_[AVERAGE_COUNT_BUFFER_SIZE];
+  uint8_t average_index_;
+  void WriteToBuffer(uint16_t level);
+  uint16_t GetBufferAverage();
+  void DisplayAudioLevelBasic(uint16_t audio_level, uint16_t max_value);
 };
+#endif  // MUSICAL_METER_H_
